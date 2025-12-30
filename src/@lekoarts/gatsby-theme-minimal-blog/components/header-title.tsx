7f666a34bfa23e056714cbaee2d26fddae4fd601
Martin ACCOU <128476349+accoumar12@users.noreply.github.com>
2025-12-30 09:20:50 +0100

    Man logo for the home button (#18)

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header-title.tsx
@@ -0,0 +1,24 @@
+/** @jsx jsx */
+import { Link } from "gatsby";
+import { jsx } from "theme-ui";
+import replaceSlashes from "@lekoarts/gatsby-theme-minimal-blog/src/utils/replaceSlashes";
+import useSiteMetadata from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-site-metadata";
+import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config";
+import { AnimatedManLogo } from "../../../components/AnimatedManLogo";
+
+const HeaderTitle = () => {
+  const { siteTitle } = useSiteMetadata();
+  const { basePath } = useMinimalBlogConfig();
+
+  return (
+    <Link
+      to={replaceSlashes(`/${basePath}`)}
+      aria-label={`${siteTitle} - Back to home`}
+      sx={{ color: `heading`, textDecoration: `none` }}
+    >
+      <AnimatedManLogo />
+    </Link>
+  );
+};
+
+export default HeaderTitle;
